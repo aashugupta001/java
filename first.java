debe9034e8eeae72a70aac61624cfa43dadc3bda
Aashu Gupta <aashugupta932@gmail.com>
2026-01-27 23:43:29 +0530

    First code in java world

---
+++ first.java
@@ -0,0 +1,7 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hello world in java ");
+        System.out.println(" by aashu \n at 26 th januray 2026");
+    }
+    
+}
