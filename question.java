e80d2c034673d2687eee771d80cf7f99cbac6c62
Aashu Gupta <aashugupta932@gmail.com>
2026-01-28 23:28:06 +0530

    how input values in java

---
+++ question.java
@@ -0,0 +1,33 @@
+
+// public class question {
+//     public static void main(String[] args) {
+//         int a=10;
+//         int b=5;
+//         int sum=a*b/a-b;
+//         System.out.println("The result is: " + sum);
+//         int ans=(a*b)/(a-b);
+//         System.out.println("The final answer is: " + ans);
+//     }
+// }
+// import java.util.*;
+// public class question{
+//     public static void main(String[] args){
+//         Scanner sc =new Scanner(System.in);
+//         String name =sc.nextLine();
+//         System.out.println(name);
+//     }
+// }
+
+import java.util.*;
+public class question{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int a =sc.nextInt();
+        // System.out.println(a);
+        int b=sc.nextInt();
+        // System.out.println(b);
+        int sum=a+b;
+        System.out.println("sum of number:"+sum);
+
+    }
+}
