228b49691e0a6516726726ffb376ed2c768d6103
Aashu Gupta <aashugupta932@gmail.com>
2026-01-28 23:28:48 +0530

    input in java

---
+++ input.java
@@ -0,0 +1,24 @@
+// public class variable {
+//     public static void main(String[] args) {
+//        int a=10;
+//          int b=20;
+//         int sum=a+b;
+//                System.out.println("the sum of a and b is :"+sum); 
+//         int diff=b-a;
+//         System.out.println("difference of b and a is :"+diff);
+//         int mult=a*b;
+//         System.out.println("multiplication of a and b is :"+mult);
+//         int div=b/a;
+//         System.out.println("division of b by a is :"+div);
+//     }
+// }
+public class input {
+    public static void main(String[] args){
+        String name="google";
+        System.out.println("the name of the company is :"+name);
+        int a=23;
+        int b=34;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
