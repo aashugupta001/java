e2d994d852f9b04d02f68307e3e660c1c0d70494
Aashu Gupta <aashugupta932@gmail.com>
2026-01-28 23:29:21 +0530

    variable assign in java

---
+++ variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main(String[] args) {
+       int a=10;
+         int b=20;
+        int sum=a+b;
+               System.out.println("the sum of a and b is :"+sum); 
+        int diff=b-a;
+        System.out.println("difference of b and a is :"+diff);
+        int mult=a*b;
+        System.out.println("multiplication of a and b is :"+mult);
+        int div=b/a;
+        System.out.println("division of b by a is :"+div);
+    }
+}
