52786656961c6eaabe8e06b832504c8bea63f9db
Aashu Gupta <aashugupta932@gmail.com>
2026-01-29 23:22:16 +0530

    if else statements

---
+++ condition.java
@@ -0,0 +1,72 @@
+// how we check condition in java for exmaple:check eligible for vote or not
+
+// import java.util.*;
+// public class condition {
+//     public static void main(String agr[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int age = sc.nextInt();
+//         if (age >18){
+//             System.out.println("eligible for vote");
+//         }
+//         else{
+//             System.out.print("not eligible for vote");
+//         }
+//         }
+
+
+
+//     }
+// import java.util.*;
+// public class condition{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
+//         if (num >= 0){
+//             System.out.println("number is positive ");
+//         }
+//         else{
+//             System.out.println("number is negative");
+//         }
+//         }
+
+//     }
+
+
+// code to check even or odd number
+// import java.util.*;
+// public class condition{
+//     public static void main(String args[]){
+//         Scanner sc =new Scanner(System.in);
+//         int number =sc.nextInt();
+//         if (number %2==0){
+//             System.out.println("number is even");
+//         }
+//         else{
+//             System.out.println("given number is odd number");
+//         }
+//         }
+//     }
+
+
+// code to find largest number among three numbers
+
+import java.util.*;
+public class condition{
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        Scanner sc2=new Scanner(System.in);
+        int b=sc.nextInt();
+        if (a>b){
+            System.out.print("a is greater");
+
+        }if(a<b){
+            System.out.println("b is graeter");
+
+        }
+        else{
+            System.out.println("a and b is eaual");
+        }
+
+    }
+}
