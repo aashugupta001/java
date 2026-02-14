7c088cda6b541da348cbaf2152aa4b4050c419f0
Aashu Gupta <aashugupta932@gmail.com>
2026-02-14 23:41:17 +0530

    using different literals

---
+++ literals.java
@@ -0,0 +1,15 @@
+public class literals {
+    public static void main(String args[]){
+        int a=10; //integer literal
+        float b=3.14f; //float literal
+        char c='A'; //character literal
+        String d="Hello"; //string literal
+        boolean e=true; //boolean literal
+
+        System.out.println("Integer literal: "+a);
+        System.out.println("Float literal: "+b);
+        System.out.println("Character literal: "+c);
+        System.out.println("String literal: "+d);
+        System.out.println("Boolean literal: "+e);
+    }
+}
