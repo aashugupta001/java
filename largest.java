cc6e421f1de4d0d2fe948f268222a23c31b5cd02
Aashu Gupta <aashugupta932@gmail.com>
2026-02-15 23:11:27 +0530

    which one is largest

---
+++ largest.java
@@ -0,0 +1,27 @@
+class LargestNumber {
+    public static void main(String[] args) {
+
+        
+        if (args.length != 3) {
+            System.out.println("Please enter exactly three numbers.");
+            return;
+        }
+
+        
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        int num3 = Integer.parseInt(args[2]);
+        
+        int largest = num1;
+
+        if (num2 > largest) {
+            largest = num2;
+        }
+        if (num3 > largest) {
+            largest = num3;
+        }
+
+        // Output result
+        System.out.println("Largest number is: " + largest);
+    }
+}
